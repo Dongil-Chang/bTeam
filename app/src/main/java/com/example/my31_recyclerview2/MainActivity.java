7953e38f082883d6ccfaff6fadc4a15ee8b3ce8a
Dongil-Chang <ssammaster@naver.com>
2021-06-22 18:23:17 +0900

    test 중입니다. int e = 0; 추가

--- app/src/main/java/com/example/my31_recyclerview2/MainActivity.java
+++ app/src/main/java/com/example/my31_recyclerview2/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 	int b = 0;
 	int c = 0;
 	int d = 0;
+	int e = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
