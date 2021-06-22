1c93b91837f070e7aa04a6227322785f0000b64d
Dongil-Chang <ssammaster@naver.com>
2021-06-22 19:50:04 +0900

    주석 삽입

--- app/src/main/java/com/example/myssproject02/Loding.java
+++ app/src/main/java/com/example/myssproject02/Loding.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+// 인트로 화면에서 2초 딜레이 후 메인 액티비티로 이동하는 클래스 파일..
 public class Loding extends AppCompatActivity {
 
     @Override
