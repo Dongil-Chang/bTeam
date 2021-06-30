70a9386f67c7d2969afe0dbb11b16501171618c2
Dongil-Chang <ssammaster@naver.com>
2021-06-30 20:49:38 +0900

    기존 패키지 폴더와 DTO 폴더를 삭제하였습니다.

--- app/src/main/java/com/example/myssproject02/OnGroupClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.myssproject02;
-
-import android.view.View;
-
-public interface OnGroupClickListener {
-    void onClick(View v);
-}
--- app/src/main/java/com/example/myssproject02/OnTextClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.myssproject02;
-
-import android.view.View;
-
-public interface OnTextClickListener {
-    void onClick(View v);
-}
